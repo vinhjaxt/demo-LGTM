33c126cbb8a7f0e503dfbf20e2dfe4c9a347c760
vinhjaxt <ngoc.lo.dang@gmail.com>
2022-09-08 23:53:53 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const { exec } = require('child_process')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  exec('ls '+req.query.file, (err, stdout, stderr) => {
+    if (err) {
+      res.send('Error')
+      return
+    }
+    res.send('OK')
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
